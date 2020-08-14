28563bdc7b6e20364d5982102f830af433d6cf26
Luis Fernando Pasquinelli Amaral de Abreu <luispasquinelli@hotmail.com>
2020-08-14 18:31:02 -0300

    Criacao do repositorio

---
+++ src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -0,0 +1,27 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns="/oi")
+public class OiMundoServlet extends HttpServlet{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		
+		PrintWriter out = res.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Oi Mundo");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("o servlet OiMundoServlet foi iniciado");
+	}
+}
