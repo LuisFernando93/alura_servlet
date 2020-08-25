925a6f2b9454a501b760254cc8aa7ce0f5a1c482
Luis Fernando Pasquinelli Amaral de Abreu <luispasquinelli@hotmail.com>
2020-08-25 00:03:43 -0300

    Aula 2: O padrao MVC

--- src/br/com/alura/gerenciador/acao/MostraEmpresa.java
+++ src/br/com/alura/gerenciador/acao/MostraEmpresa.java
@@ -10,9 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
-public class MostraEmpresa {
+public class MostraEmpresa implements Acao {
 	
-	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		System.out.println("mostrando dados da empresa");
 		
@@ -24,7 +24,7 @@ public class MostraEmpresa {
 		Empresa empresa = banco.buscaEmpresaById(id);
 		
 		request.setAttribute("empresa", empresa);
-		RequestDispatcher rd = request.getRequestDispatcher("/formAlteraEmpresa.jsp");
-		rd.forward(request, response);
+		
+		return "forward:formAlteraEmpresa.jsp";
 	}
 }
--- src/br/com/alura/gerenciador/acao/NovaEmpresa.java
+++ src/br/com/alura/gerenciador/acao/NovaEmpresa.java
@@ -11,9 +11,9 @@ import javax.servlet.http.HttpServletResponse;
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
-public class NovaEmpresa {
+public class NovaEmpresa implements Acao {
 
-	public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
 		
 		System.out.println("Cadastrando nova empresa");
 		
@@ -36,6 +36,6 @@ public class NovaEmpresa {
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
-		response.sendRedirect("entrada?acao=ListaEmpresas");
+		return "redirect:entrada?acao=ListaEmpresas";
 	}
 }
