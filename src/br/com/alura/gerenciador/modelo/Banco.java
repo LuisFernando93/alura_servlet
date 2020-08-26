155068775a43bf58b688162044df6d9e19406755
Luis Fernando Pasquinelli Amaral de Abreu <luispasquinelli@hotmail.com>
2020-08-26 18:16:31 -0300

    Aula 3: formulario de login

--- src/br/com/alura/gerenciador/modelo/Banco.java
+++ src/br/com/alura/gerenciador/modelo/Banco.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static List<Usuario> listaUsuarios = new ArrayList<Usuario>();
 	private static Integer chaveSequencial = 1; 
 	
 	static {
@@ -18,6 +19,20 @@ public class Banco {
 		empresa2.setNome("Caelum");
 		lista.add(empresa1);
 		lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("luis");
+		u1.setSenha("1234");
+		
+		Usuario u2 = new Usuario();
+		u2.setLogin("julia");
+		u2.setSenha("1234");
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
+		
+		System.out.println(u1.getLogin() + " | " + u1.getSenha());
+		System.out.println(u2.getLogin() + " | " + u2.getSenha());
 	}
 	
 	public void adiciona(Empresa empresa) {
@@ -50,6 +65,16 @@ public class Banco {
 			}
 		}
 		
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		
+		for (Usuario usuario : listaUsuarios) {
+			if(usuario.ehIgual(login, senha)) {
+				return usuario;
+			}
+		}
 		return null;
 	}
 
